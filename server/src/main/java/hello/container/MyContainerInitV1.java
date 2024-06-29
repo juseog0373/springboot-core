aee4c1502e85228f1bd9ce8b2a606b5de86bf4a3
jskim <juseog0373@naver.com>
2024-06-29 21:32:32 +0900

    서블릿 컨테이너 초기화2

--- server/src/main/java/hello/container/MyContainerInitV1.java
+++ server/src/main/java/hello/container/MyContainerInitV1.java
@@ -7,7 +7,6 @@ import jakarta.servlet.ServletException;
 import java.util.Set;
 
 public class MyContainerInitV1 implements ServletContainerInitializer {
-
     @Override
     public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
         System.out.println("MyContainerInitV1.onStartup");
